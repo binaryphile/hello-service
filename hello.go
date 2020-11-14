067bbead3d5fc2f49a47116d8ccae4b9afffcf7a
Ted Lilley <ted.lilley@gmail.com>
2020-11-14 03:59:12 +0000

    test

--- hello.go
+++ hello.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	"fmt"
+	"html"
+	"log"
+	"net/http"
 
 	"github.com/binaryphile/hello-service/services/msgsvc"
 )
 
 func main() {
-	fmt.Println(msgsvc.GetMessage())
+	http.HandleFunc("/msg", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "%s\n", html.EscapeString(msgsvc.GetMessage()))
+	})
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
